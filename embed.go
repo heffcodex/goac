a42839e320068374c9eb36b89185133614c04844
heffcodex <dev@heffcodex.pro>
2022-12-09 02:37:05 +0400

    init

--- embed.go
+++ embed.go
@@ -1,9 +1,43 @@
 package goac
 
-type Embed struct {
-	Permissions []Path `json:"__permissions,omitempty"`
+import "encoding/json"
+
+type IEmbed interface {
+	json.Marshaler
+	GetOAP() IObject
+	SetOAP(o IObject)
+	UpdateOAPPermissions()
 }
 
-func (e *Embed) SetOAPPermissions(o IObject) {
-	e.Permissions = o.GetAllowedPaths()
+var _ IEmbed = (*Embed)(nil)
+
+type Embed struct {
+	Permissions []Path `json:"__permissions,omitempty"`
+	o           IObject
+}
+
+func (e *Embed) GetOAP() IObject {
+	return e.o
+}
+
+func (e *Embed) SetOAP(o IObject) {
+	e.o = o
+}
+
+func (e *Embed) UpdateOAPPermissions() {
+	if e.o == nil {
+		panic("oap is not set")
+	}
+
+	e.Permissions = e.o.GetAllowedPaths()
+}
+
+func (e *Embed) MarshalJSON() ([]byte, error) {
+	e.UpdateOAPPermissions()
+
+	if len(e.Permissions) == 0 {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(e.Permissions)
 }
