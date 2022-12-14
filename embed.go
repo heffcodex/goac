7da2123f280ef5de2c7eaa994a9c9785c85b5df2
heffcodex <dev@heffcodex.pro>
2022-12-14 07:04:22 +0400

    init

--- embed.go
+++ embed.go
@@ -1,13 +1,31 @@
 package goac
 
-import "github.com/heffcodex/goac/oap"
+import (
+	"context"
+	"github.com/heffcodex/goac/oap"
+)
+
+type IEmbed interface {
+	GetPermissions() []goacoap.Path
+	SetPermissions([]goacoap.Path)
+}
+
+var _ IEmbed = (*Embed)(nil)
 
 type Embed struct {
 	Permissions []goacoap.Path `json:"__permissions,omitempty"`
 }
 
-func NewEmbed(o goacoap.IObject) Embed {
-	return Embed{
-		Permissions: o.GetAllowedPaths(),
-	}
+func (e *Embed) GetPermissions() []goacoap.Path {
+	return e.Permissions
+}
+
+func (e *Embed) SetPermissions(p []goacoap.Path) {
+	e.Permissions = p
+}
+
+type ObjectEnforcer[T IEmbed] func(ctx context.Context, o T) (T, error)
+
+func (e ObjectEnforcer[T]) FromContext(ctx context.Context, key string) ObjectEnforcer[T] {
+	return ctx.Value(key).(ObjectEnforcer[T])
 }
